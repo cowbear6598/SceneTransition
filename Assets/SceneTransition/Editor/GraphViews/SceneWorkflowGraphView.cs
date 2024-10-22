802c6ccaefa53b123cf904ee88edc6d1741111e9
CptSoap <cowbear6598@gmail.com>
2024-10-22 17:07:21 +0800

    :construction: complete open window and graph view

---
+++ Assets/SceneTransition/Editor/GraphViews/SceneWorkflowGraphView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace SceneTransition.Editor.GraphViews
+{
+	public class SceneWorkflowGraphView : GraphView
+	{
+		public SceneWorkflowGraphView()
+		{
+			this.AddManipulator(new ContentDragger());
+			this.AddManipulator(new SelectionDragger());
+			this.AddManipulator(new RectangleSelector());
+			this.AddManipulator(new ContentZoomer());
+
+			var grid = new GridBackground();
+			Insert(0, grid);
+			grid.StretchToParentSize();
+
+			style.flexGrow   = 1;
+			style.flexShrink = 1;
+		}
+
+		public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+		{
+			var compatiblePorts = new List<Port>();
+
+			ports.ForEach(port =>
+			{
+				if (startPort.node == port.node)
+					return;
+
+				if (startPort.direction == port.direction)
+					return;
+
+				compatiblePorts.Add(port);
+			});
+
+			return compatiblePorts;
+		}
+	}
+}
---
+++ Assets/SceneTransition/Editor/Windows/SceneWorkflowEditorWindow.cs
@@ -0,0 +1,39 @@
+﻿using SceneTransition.Editor.GraphViews;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SceneTransition.Editor.Windows
+{
+	public class SceneWorkflowEditorWindow : EditorWindow
+	{
+		private SceneWorkflowGraphView _graphView;
+
+		[MenuItem("SceneTransition/Workflow Editor")]
+		public static void OpenWindow()
+		{
+			var window = GetWindow<SceneWorkflowEditorWindow>();
+			window.titleContent = new GUIContent("Scene Workflow Editor");
+		}
+
+		private void OnEnable()
+		{
+			var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/SceneTransition/Editor/GraphViews/SceneWorkflowGraphViewCss.uss");
+
+			if (styleSheet != null)
+				rootVisualElement.styleSheets.Add(styleSheet);
+
+			_graphView                = new SceneWorkflowGraphView();
+			_graphView.style.flexGrow = 1;
+
+			_graphView.AddToClassList("workflow-graph-view");
+
+			rootVisualElement.Add(_graphView);
+		}
+
+		private void OnDisable()
+		{
+			rootVisualElement.Remove(_graphView);
+		}
+	}
+}
