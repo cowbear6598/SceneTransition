e5dd789b4e3a019a1943b3d6b2e9a7ef6b6f7ee2
CptSoap <cowbear6598@gmail.com>
2024-10-24 16:20:29 +0800

    :sparkles: make asset to read only

---
+++ Assets/SceneTransition/Editor/Windows/SceneWorkflowAssetEditor.cs
@@ -0,0 +1,18 @@
+﻿using SceneTransition.ScriptableObjects;
+using UnityEditor;
+
+namespace SceneTransition.Editor.Windows
+{
+	[CustomEditor(typeof(SceneWorkflowAsset))]
+	public class SceneWorkflowAssetEditor : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("_operationData"));
+			EditorGUI.EndDisabledGroup();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
